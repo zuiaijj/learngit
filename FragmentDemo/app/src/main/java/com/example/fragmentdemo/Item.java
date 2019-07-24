b8789a27a1e3170dc716e2397b3e74470811ed44
QianCong <qianconggit@163.com>
2019-07-24 15:29:37 +0800

    7.24 Fragment+TabLayout+RecyclerView v1

---
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/Item.java
@@ -0,0 +1,77 @@
+package com.example.fragmentdemo;
+
+
+public class Item {
+    private String name;
+    private String golden_item_text = null;
+    private String sliver_item_text = null;
+    private int icon_imageId;
+    private boolean isHead = false;
+    private int liwubang_imageId1;
+    private int liwubang_imageId2;
+    private int liwubang_imageId3;
+
+    public Item(String name, int icon_imageId, int liwubang_imageId1, int liwubang_imageId2, int liwubang_imageId3, String golden_item_text, String sliver_item_text, boolean isHead) {
+        this.name = name;
+        this.golden_item_text = golden_item_text;
+        this.sliver_item_text = sliver_item_text;
+        this.icon_imageId = icon_imageId;
+        this.isHead = isHead;
+        this.liwubang_imageId1 = liwubang_imageId1;
+        this.liwubang_imageId2 = liwubang_imageId2;
+        this.liwubang_imageId3 = liwubang_imageId3;
+    }
+
+    public Item(String name, int icon_imageId, int liwubang_imageId1, int liwubang_imageId2, int liwubang_imageId3) {
+        this.name = name;
+        this.icon_imageId = icon_imageId;
+        this.liwubang_imageId1 = liwubang_imageId1;
+        this.liwubang_imageId2 = liwubang_imageId2;
+        this.liwubang_imageId3 = liwubang_imageId3;
+    }
+
+    public Item(String name, int icon_imageId, String golden_item_text, String sliver_item_text) {
+        this.name = name;
+        this.golden_item_text = golden_item_text;
+        this.sliver_item_text = sliver_item_text;
+        this.icon_imageId = icon_imageId;
+    }
+
+    public Item(String name, int icon_imageId) {
+        this.name = name;
+        this.icon_imageId = icon_imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getgolden_item_text() {
+        return golden_item_text;
+    }
+
+    public String getsliver_item_text() {
+        return sliver_item_text;
+    }
+
+    public int getImageId() {
+        return icon_imageId;
+    }
+
+    public boolean getisHead() {
+        return isHead;
+    }
+
+    public int getliwubang_imageId1() {
+        return liwubang_imageId1;
+    }
+
+    public int getliwubang_imageId2() {
+        return liwubang_imageId2;
+    }
+
+    public int getliwubang_imageId3() {
+        return liwubang_imageId3;
+    }
+}
+
---
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/RecycleItemAdapter.java
@@ -0,0 +1,106 @@
+package com.example.fragmentdemo;
+
+//import androidx.appcompat.app.AlertController;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecycleItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static final int ITEM_TYPE_HEADER = 0;
+    public static final int ITEM_TYPE_CONTENT = 1;
+    private int mHeaderCount = 1;
+    private Context mContext;
+    private List<Item> itemList;
+
+    public RecycleItemAdapter(Context context, List<Item> itemList) {
+        this.mContext = context;
+        this.itemList = itemList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mHeaderCount != 0 && position < mHeaderCount) {
+            return ITEM_TYPE_HEADER;
+        } else {
+            return ITEM_TYPE_CONTENT;
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == ITEM_TYPE_HEADER) {
+            View headView = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+            headViewHolder holder = new headViewHolder(headView);
+            return holder;
+        } else if (viewType == ITEM_TYPE_CONTENT) {
+            View contentView = LayoutInflater.from(mContext).inflate(R.layout.item, parent, false);
+            contentViewHolder holder = new contentViewHolder(contentView);
+            return holder;
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        if (holder instanceof headViewHolder) {
+
+        } else if (holder instanceof contentViewHolder) {
+            ((contentViewHolder) holder).itemNameText.setText(item.getName());
+            ((contentViewHolder) holder).goldenText.setText(item.getgolden_item_text());
+            ((contentViewHolder) holder).sliverText.setText(item.getsliver_item_text());
+            ((contentViewHolder) holder).itemImageView.setImageResource(item.getImageId());
+            ((contentViewHolder) holder).liWuBangItemImageView1.setImageResource(item.getliwubang_imageId1());
+            ((contentViewHolder) holder).liWuBangItemImageView2.setImageResource(item.getliwubang_imageId2());
+            ((contentViewHolder) holder).liWuBangItemImageView3.setImageResource(item.getliwubang_imageId3());
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position, @NonNull List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    static class contentViewHolder extends RecyclerView.ViewHolder {
+        private TextView itemNameText;
+        private TextView goldenText;
+        private TextView sliverText;
+        private ImageView itemImageView;
+        private ImageView liWuBangItemImageView1;
+        private ImageView liWuBangItemImageView2;
+        private ImageView liWuBangItemImageView3;
+
+        contentViewHolder(View contentView) {
+            super(contentView);
+            itemNameText = (TextView) contentView.findViewById(R.id.list_textview);
+            goldenText = (TextView) contentView.findViewById(R.id.golden_item_text_tv);
+            sliverText = (TextView) contentView.findViewById(R.id.sliver_item_text_tv);
+            itemImageView = (ImageView) contentView.findViewById(R.id.list_icon);
+            liWuBangItemImageView1 = (ImageView) contentView.findViewById(R.id.liwubang_iv1);
+            liWuBangItemImageView2 = (ImageView) contentView.findViewById(R.id.liwubang_iv2);
+            liWuBangItemImageView3 = (ImageView) contentView.findViewById(R.id.liwubang_iv3);
+        }
+    }
+
+    static class headViewHolder extends RecyclerView.ViewHolder {
+        headViewHolder(View headView) {
+            super(headView);
+        }
+
+    }
+}
