3113ae068ef78cf39f78281b37137838d8f106fb
QianCong <qianconggit@163.com>
2019-07-29 14:54:15 +0800

    7.29 formate

--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/MineFragment.java
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/MineFragment.java
@@ -23,26 +23,29 @@ public class MineFragment extends Fragment {
     private TextView timeTv;
     private static final int START_TIME = 1;
     private static final int STOP_TIME = 0;
+    private Timer timer;
+    private CountHandler handler;
+
 
     private static class CountHandler extends Handler {
         private final WeakReference<TextView> target;
 
-        public CountHandler(TextView timeTv) {
-            target = new WeakReference<TextView>(timeTv);
+        CountHandler(TextView timeTv) {
+            target = new WeakReference<>(timeTv);
         }
 
         @Override
         public void handleMessage(@NonNull Message msg) {
-            TextView handerTv = target.get();
-            if (handerTv != null) {
+            TextView handlerTv = target.get();
+            if (handlerTv != null) {
                 super.handleMessage(msg);
                 switch (msg.what) {
                     case START_TIME:
-                        handerTv.setText("倒计时" + msg.arg1);
+                        handlerTv.setText(R.string.倒计时 + msg.arg1);
                         Log.d("time", "timechange" + msg.arg1);
                         break;
                     case STOP_TIME:
-                        handerTv.setText("开始倒计时");
+                        handlerTv.setText(R.string.开始倒计时);
                         System.gc();
                         break;
                 }
@@ -58,13 +61,11 @@ public class MineFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mine_fragment, container, false);
         timeTv = view.findViewById(R.id.time_tx);
-        timeTv.setText("开始倒计时");
+        timeTv.setText(R.string.开始倒计时);
         return view;
 
     }
 
-    private Timer timer;
-    private CountHandler handler;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -91,13 +92,13 @@ public class MineFragment extends Fragment {
                         if (time >= 0) {
                             message.what = START_TIME;
                             message.arg1 = time;
-                            if(handler!=null) {
+                            if (handler != null) {
                                 handler.sendMessage(message);
                             }
                             time--;
                         } else {
                             message.what = STOP_TIME;
-                            if(handler!=null) {
+                            if (handler != null) {
                                 handler.sendMessage(message);
                             }
                             timer.cancel();
@@ -112,10 +113,9 @@ public class MineFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        if(timer!=null) {
+        if (timer != null) {
             timer.cancel();
         }
-        System.gc();
         handler.removeCallbacksAndMessages(null);
         Log.d("time", handler.toString() + "onDetach");
 //            handler.removeCallbacksAndMessages(null);
